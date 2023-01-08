66aeea219382a2360d808a4e7a8afd56508520f1
emmannocedo <106206034+emmannocedo@users.noreply.github.com>
2023-01-08 21:47:11 +0800

    Update script.js

--- script.js
+++ script.js
@@ -11,9 +11,10 @@ const observer = new IntersectionObserver((entries) => {
         console.log(entry);
         if (entry.isIntersecting) {
             entry.target.classList.add("show");
-        } else {
-            entry.target.classList.remove("show");
-        }
+        } 
+//         else {
+//             entry.target.classList.remove("show");
+//         }
     })
 });
 
