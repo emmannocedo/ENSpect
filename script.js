41905e0a77f9fee1ca55243bf9caafd6134dba29
emmannocedo <106206034+emmannocedo@users.noreply.github.com>
2023-01-08 19:41:47 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const hamburgerIcon = document.querySelector('.hamburger');
+const navList = document.querySelector('header');
+
+hamburgerIcon.addEventListener('click', () => {
+    navList.classList.toggle('toggle');
+    hamburgerIcon.classList.toggle('active');
+});
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry);
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        } else {
+            entry.target.classList.remove("show");
+        }
+    })
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
